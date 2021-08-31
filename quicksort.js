299598c0b7d053c10032d49c4dd46e9356d2397d
Aayush <aayushchhajed@gmail.com>
2021-08-31 19:59:15 +0530

    added data-set

--- quicksort.js
+++ quicksort.js
@@ -8,7 +8,7 @@ function partition(array, low, high) {
             let temp = array[i];
             arr[i] = arr[j];
             arr[j] = temp;
-            console.log("pivot=", pivot, "array[j]", array[j], "array[i]", array[i], array);
+            console.log("pivot=", pivot, "array[j]", array[j], "array[i]", array[i], JSON.stringify(array));
         }
     }
     return i;
@@ -18,13 +18,17 @@ function quicksort(array, lo, hi) {
     if (lo >= 0 && hi >= 0) {
         if (lo < hi) {
             p = partition(array, lo, hi);
+            //left hand side
+            console.log("LHS",JSON.stringify(array.slice(lo,p-1)))
             quicksort(array, lo, p - 1);
+            //right hand side
+            console.log("RHS",JSON.stringify(array.slice(p+1,hi)))
             quicksort(array, p + 1, hi);
-            console.log(p)
+            // console.log(p)
         }
     }
 }
 
-let arr = [8, 9, 1, 2, 7, 4];
+let arr = [895,234,654,342,564,634,432,237,543,876,868,908,543];
 console.log(arr);
 quicksort(arr, 0, arr.length - 1);
